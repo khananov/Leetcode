7451a14962dba7d837a5d2d533362425d63260d0
khananov <khananovtf@gmail.com>
2023-10-13 21:35:28 +0300

    746.Min cost climbing stairs solution

---
+++ src/main/java/ru/khananov/MinCostClimbingStairs.java
@@ -0,0 +1,21 @@
+package ru.khananov;
+
+public class MinCostClimbingStairs {
+    public static void main(String[] args) {
+        new MinCostClimbingStairs().minCostClimbingStairs(
+                new int[] {1,100,1,1,1,100,1,1,100,1}
+        );
+    }
+
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+
+        return dp[n];
+    }
+
+}
