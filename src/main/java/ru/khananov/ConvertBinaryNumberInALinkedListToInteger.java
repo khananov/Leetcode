238303f3d55fc7270f12084a293b30076840ea4b
khananov <khananovtf@gmail.com>
2023-11-12 21:59:47 +0300

    1290. Convert Binary Number in a Linked List to Integer solution

--- src/main/java/ru/khananov/ConvertBinaryNumberInALinkedListToInteger.java
+++ src/main/java/ru/khananov/ConvertBinaryNumberInALinkedListToInteger.java
@@ -1,4 +1,40 @@
 package ru.khananov;
 
 public class ConvertBinaryNumberInALinkedListToInteger {
+    public static void main(String[] args) {
+        System.out.println(new ConvertBinaryNumberInALinkedListToInteger().getDecimalValue(new ListNode(
+                1, new ListNode(
+                0, new ListNode(
+                0, new ListNode(
+                1, new ListNode(
+                0, new ListNode(
+                0, new ListNode(
+                1, new ListNode(
+                1, new ListNode(
+                1, new ListNode(
+                0, new ListNode(
+                0, new ListNode(
+                0, new ListNode(
+                0, new ListNode(
+                0, new ListNode(
+                0)))))))))))))))));
+    }
+
+    public int getDecimalValue(ListNode head) {
+        ListNode temp = head;
+        int decNum = 0;
+        int powerOfNum = -1;
+
+        while (temp != null) {
+            powerOfNum++;
+            temp = temp.next;
+        }
+
+        while (powerOfNum >= 0 && head != null) {
+            decNum += (int) Math.pow(2, powerOfNum--) * head.val;
+            head = head.next;
+        }
+
+        return decNum;
+    }
 }
